3dd5eddccca0d3d1b54522331a491e464947dd5b
Jo Vanvoorden <jo.vanvoorden@kuleuven.be>
2015-09-04 11:49:12 +0200

    Set the management port in the global configuration

--- set_rabbitmq_password.sh
+++ set_rabbitmq_password.sh
@@ -7,11 +7,17 @@ fi
 
 PASS=${RABBITMQ_PASS:-$(pwgen -s 12 1)}
 USER=${RABBITMQ_USER:-"admin"}
+MGMTPORT=${RABBITMQ_MGMT_PORT:-15672}
 _word=$( [ ${RABBITMQ_PASS} ] && echo "preset" || echo "random" )
 echo "=> Securing RabbitMQ with a ${_word} password"
 cat > /etc/rabbitmq/rabbitmq.config <<EOF
 [
 	{rabbit, [{default_user, <<"$USER">>},{default_pass, <<"$PASS">>},{tcp_listeners, [{"::", 5672}]}]}
+	{rabbitmq_management, 
+		[{listener, [{port,$MGMTPORT},
+                 {ip, "::"}
+								]}
+	  ]}
 ].
 EOF
 
