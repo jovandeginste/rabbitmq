84b1accb51c2a8dacd319d58e8be7d73ed817f70
Jo Vanvoorden <jo.vanvoorden@kuleuven.be>
2015-09-17 10:56:25 +0200

    Add ipset for ipv6 firewall rules Add the epmd port in the containerports ipset to make it available for other nodes

--- Dockerfile
+++ Dockerfile
@@ -5,7 +5,7 @@ MAINTAINER Fernando Mayo <fernando@tutum.co>
 RUN apt-key adv --keyserver keyserver.ubuntu.com --recv-keys F7B8CEA6056E8E56 && \
     echo "deb http://www.rabbitmq.com/debian/ testing main" >> /etc/apt/sources.list && \
     apt-get update && \
-    apt-get install -y rabbitmq-server pwgen && \
+    apt-get install -y rabbitmq-server pwgen ipset && \
     rabbitmq-plugins enable rabbitmq_management && \
     rabbitmq-plugins enable rabbitmq_stomp && \
     apt-get clean && \
--- run.sh
+++ run.sh
@@ -6,11 +6,17 @@ if [ ! -f /.rabbitmq_password_set ]; then
 	/set_rabbitmq_password.sh
 fi
 
+IPV6=`ip -6 addr list dev eth0 | grep global | sed "s/.*inet6 //;s+/80.*++"`
+
 # make rabbit own its own files
 chown -R rabbitmq:rabbitmq /var/lib/rabbitmq
 
 if [ -z "$CLUSTER_WITH" ] ; then
-    /usr/sbin/rabbitmq-server
+    /usr/sbin/rabbitmq-server &
+    sleep 5
+    PORT=`epmd -names | sed '/rabbit/!d;s/.*port //'`
+    ipset add containerports ${IPV6},tcp:$PORT
+    fg
 else
     if [ -f /.CLUSTERED ] ; then
     /usr/sbin/rabbitmq-server
@@ -18,6 +24,8 @@ else
         touch /.CLUSTERED
         /usr/sbin/rabbitmq-server &
         sleep 10
+        PORT=`epmd -names | sed '/rabbit/!d;s/.*port //'`
+        ipset add containerports ${IPV6},tcp:$PORT
         rabbitmqctl stop_app
         rabbitmqctl join_cluster rabbit@$CLUSTER_WITH
         rabbitmqctl start_app
