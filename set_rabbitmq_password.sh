afeac30e23c64a0c23af11f45c0c715fceebbd28
Jo Vandeginste <Jo.Vandeginste@kuleuven.be>
2018-03-19 10:53:13 +0100

    Reformat shell scripts and remove ipset stuff

--- set_rabbitmq_password.sh
+++ set_rabbitmq_password.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 if [ -f /config/.rabbitmq_password_set ]; then
-   echo "RabbitMQ password already set!"
-   exit 0
+	echo "RabbitMQ password already set!"
+	exit 0
 fi
 
 PASS=${RABBITMQ_PASS:-$(pwgen -s 12 1)}
@@ -11,28 +11,28 @@ MGMTPORT=${RABBITMQ_MGMT_PORT:-15672}
 STOMPPORT=${RABBITMQ_STOMP_PORT:-61613}
 ERLANG_COOKIE=${RABBITMQ_ERLANG_COOKIE:-ERLANGCOOKIE}
 
-_word=$( [ ${RABBITMQ_PASS} ] && echo "preset" || echo "random" )
+_word=$([ ${RABBITMQ_PASS} ] && echo "preset" || echo "random")
 echo "=> Securing RabbitMQ with a ${_word} password"
-cat > /etc/rabbitmq/rabbitmq.config <<EOF
+cat >/etc/rabbitmq/rabbitmq.config <<EOF
 [
    {rabbit, [{default_user, <<"$USER">>},{default_pass, <<"$PASS">>},{tcp_listeners, [{"::", 5672}]}]},
    {rabbitmq_stomp, [{tcp_listeners, [{"::", $STOMPPORT}]},
-                     {implicit_connect, true}
-                    ]},
-   {rabbitmq_management, 
+		     {implicit_connect, true}
+		    ]},
+   {rabbitmq_management,
       [{listener, [{port,$MGMTPORT},
-                 {ip, "::"}
-                        ]}
+		 {ip, "::"}
+			]}
     ]}
 ].
 EOF
 echo "=> Setting erlang cookie"
-echo $ERLANG_COOKIE > /var/lib/rabbitmq/.erlang.cookie
+echo $ERLANG_COOKIE >/var/lib/rabbitmq/.erlang.cookie
 echo "=> configuring plugins"
-cat > /etc/rabbitmq/enabled_plugins <<EOF
+cat >/etc/rabbitmq/enabled_plugins <<EOF
 [rabbitmq_management,rabbitmq_stomp].
 EOF
-chown rabbitmq:rabbitmq /var/lib/rabbitmq/.erlang.cookie /etc/rabbitmq/* 
+chown rabbitmq:rabbitmq /var/lib/rabbitmq/.erlang.cookie /etc/rabbitmq/*
 chmod 400 /var/lib/rabbitmq/.erlang.cookie /etc/rabbitmq/*
 echo "=> Done!"
 touch /config/.rabbitmq_password_set
@@ -42,12 +42,12 @@ echo "You can now connect to this RabbitMQ server using, for example:"
 echo ""
 
 if [ ${_word} == "random" ]; then
-    echo "    curl --user $USER:$PASS http://<host>:<port>/api/vhosts"
-    echo ""
-    echo "Please remember to change the above password as soon as possible!"
+	echo "    curl --user $USER:$PASS http://<host>:<port>/api/vhosts"
+	echo ""
+	echo "Please remember to change the above password as soon as possible!"
 else
-    echo "    curl --user $USER:<RABBITMQ_PASS> http://<host>:<port>/api/vhosts"
-    echo ""
+	echo "    curl --user $USER:<RABBITMQ_PASS> http://<host>:<port>/api/vhosts"
+	echo ""
 fi
 
 echo "========================================================================"
