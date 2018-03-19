4545fa1aeb47bc07e2ecda5f097ec6631e90ec9b
Jo Vandeginste <Jo.Vandeginste@kuleuven.be>
2018-03-19 11:41:47 +0100

    Rework the whole script+Dockerfile

--- run.sh
+++ run.sh
@@ -2,14 +2,23 @@
 
 set -m
 
-if [ ! -f /config/.rabbitmq_password_set ]; then
+: ${DNS_DELAY:=60}
+: ${CLUSTER_WITH:=}
+
+if [[ ! -f /config/.rabbitmq_password_set ]]; then
 	/set_rabbitmq_password.sh
+fi
+
+if [[ ! -f /var/lib/rabbitmq/.erlang.cookie ]]; then
+	echo "ERLANGCOOKIE" >/var/lib/rabbitmq/.erlang.cookie
+	chown rabbitmq:rabbitmq /var/lib/rabbitmq/.erlang.cookie
+	chmod 400 /var/lib/rabbitmq/.erlang.cookie
 fi
 
 # make rabbit own its own files
 chown -R rabbitmq:rabbitmq /var/lib/rabbitmq
 
-if [ -z "$CLUSTER_WITH" ]; then
+if [ -z "${CLUSTER_WITH}" ]; then
 	/usr/sbin/rabbitmq-server
 else
 	if [ -f /config/.CLUSTERED ]; then
@@ -17,11 +26,31 @@ else
 		/usr/sbin/rabbitmq-server
 	else
 		/usr/sbin/rabbitmq-server &
-		sleep 10
+
+		echo -n "Trying to resolve '${CLUSTER_WITH}' (max. ${DNS_DELAY} seconds.) "
+		for i in {1..${DNS_DELAY}}; do
+			echo -n "."
+			getent hosts "${CLUSTER_WITH}" && break
+		done
+
+		echo
+
+		if getent hosts "${CLUSTER_WITH}"; then
+			echo "DNS resolution of '${CLUSTER_WITH}' successful."
+		else
+			echo "Failed to resolve DNS for '${CLUSTER_WITH}' within ${DNS_DELAY} seconds. Bailing out."
+			exit 1
+		fi
+
 		rabbitmqctl stop_app
-		rabbitmqctl join_cluster rabbit@$CLUSTER_WITH
+		if rabbitmqctl join_cluster rabbit@${CLUSTER_WITH}; then
+			echo "Clustering with 'rabbit@${CLUSTER_WITH}' was successful."
+			touch /config/.CLUSTERED
+		else
+			echo "Failed to cluster with 'rabbit@${CLUSTER_WITH}'. Bailing out."
+			exit 1
+		fi
 		rabbitmqctl start_app
-		touch /config/.CLUSTERED
 		fg
 	fi
 fi
